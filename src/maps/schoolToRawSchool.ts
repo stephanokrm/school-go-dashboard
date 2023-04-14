4868128b3ff2875bbff55889ad99be7061e6e3d4
Stephano Ramos <stephano.ramos@uber.com>
2023-04-13 22:38:07 -0300

    formats time

--- src/maps/schoolToRawSchool.ts
+++ src/maps/schoolToRawSchool.ts
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import { RawSchool, School } from "../types";
 import { addressToRawAddress } from "./addressToRawAddress";
 
@@ -9,11 +10,23 @@ export const schoolToRawSchool = async (
   morning: school.morning,
   afternoon: school.afternoon,
   night: school.night,
-  morning_entry_time: school.morningEntryTime,
-  morning_departure_time: school.morningDepartureTime,
-  afternoon_entry_time: school.afternoonEntryTime,
-  afternoon_departure_time: school.afternoonDepartureTime,
-  night_entry_time: school.nightEntryTime,
-  night_departure_time: school.nightDepartureTime,
+  morning_entry_time: school.morningEntryTime
+    ? format(school.morningEntryTime, "HH:mm:ss")
+    : undefined,
+  morning_departure_time: school.morningDepartureTime
+    ? format(school.morningDepartureTime, "HH:mm:ss")
+    : undefined,
+  afternoon_entry_time: school.afternoonEntryTime
+    ? format(school.afternoonEntryTime, "HH:mm:ss")
+    : undefined,
+  afternoon_departure_time: school.afternoonDepartureTime
+    ? format(school.afternoonDepartureTime, "HH:mm:ss")
+    : undefined,
+  night_entry_time: school.nightEntryTime
+    ? format(school.nightEntryTime, "HH:mm:ss")
+    : undefined,
+  night_departure_time: school.nightDepartureTime
+    ? format(school.nightDepartureTime, "HH:mm:ss")
+    : undefined,
   address: await addressToRawAddress(school.address),
 });
