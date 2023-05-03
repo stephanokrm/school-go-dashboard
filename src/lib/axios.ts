800a6e33e24b75c84c436bad1f0f32a1efc315a6
Stephano Ramos <stephano.ramos@uber.com>
2023-05-03 18:15:50 -0300

    axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,7 @@ import Axios from "axios";
 const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
+    Accept: "application/json",
     "X-Requested-With": "XMLHttpRequest",
   },
   withCredentials: true,
