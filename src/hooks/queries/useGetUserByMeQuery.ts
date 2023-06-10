7d26d52797ab28780fd327e8c30f8647764a65a8
Stephano Ramos <stephano.ramos@uber.com>
2023-06-10 14:35:50 -0300

    refactors

--- src/hooks/queries/useGetUserByMeQuery.ts
+++ src/hooks/queries/useGetUserByMeQuery.ts
@@ -1,7 +1,7 @@
 import { AxiosError } from "axios";
-import { useQuery } from "@tanstack/react-query";
-import { getUserByMe } from "../../services/getUserByMe";
 import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+import { getUserByMe } from "@/services/getUserByMe";
 
 export const useGetUserByMeQuery = () => {
   const router = useRouter();
@@ -12,6 +12,8 @@ export const useGetUserByMeQuery = () => {
       return getUserByMe({ signal });
     },
     {
+      retry: false,
+      retryOnMount: false,
       onError: async (error: AxiosError) => {
         if (error.response?.status !== 409) throw error;
 
