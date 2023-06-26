78359371cfa9f86ced9dd0c37a99111a9686e706
Stephano Ramos <stephano.ramos@uber.com>
2023-06-26 20:12:13 -0300

    refactors

--- src/hooks/queries/useTripsQuery.ts
+++ src/hooks/queries/useTripsQuery.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "../../lib/axios";
-import { RawTrip, Resource } from "../../types";
-import { rawTripToTrip } from "../../maps/rawTripToTrip";
+import { RawTrip, Resource } from "@/types";
+import { rawTripToTrip } from "@/maps/rawTripToTrip";
 
 interface Params {
   driver?: boolean;
@@ -9,14 +9,20 @@ interface Params {
 }
 
 export const useTripsQuery = (params: Params = {}) => {
-  return useQuery(["Trips"], async ({ signal }) => {
-    const {
-      data: { data: rawTrip },
-    } = await axios.get<Resource<RawTrip[]>>(`/api/trip`, {
-      params,
-      signal,
-    });
+  return useQuery(
+    ["Trips"],
+    async ({ signal }) => {
+      const {
+        data: { data: rawTrip },
+      } = await axios.get<Resource<RawTrip[]>>(`/api/trip`, {
+        params,
+        signal,
+      });
 
-    return Promise.all(rawTrip.map(rawTripToTrip));
-  });
+      return Promise.all(rawTrip.map(rawTripToTrip));
+    },
+    {
+      refetchInterval: 30000,
+    }
+  );
 };
